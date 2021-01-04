e846c2c0ed761f0f125ef899e29bf2c7c211066b
VUTuanTrung <trung.lespace@gmail.com>
2021-01-04 17:20:13 +0100

    using Link

--- blog/src/containers/Blog/Blog.js
+++ blog/src/containers/Blog/Blog.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 
 //import axios from 'axios';
 import './Blog.css';
+import { Route, Link } from 'react-router-dom';
 import Posts from './Posts/Posts';
-import { Route } from 'react-router-dom';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
 
@@ -13,13 +14,15 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to='/'>Home</Link></li>
+                            <li><Link to={{
+                                pathname: '/new-post',
+                            }}>New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
                 <Route path='/' exact component={Posts} />
-                {/* <Route path='/' exact render={Posts} Component={Posts} /> */}
+                <Route path='/new-post' exact component={NewPost} />
             </div>
         );
     }
