a8511c8825e5ed3762e83122c76d622935b596e5
VUTuanTrung <trung.lespace@gmail.com>
2021-01-04 13:06:48 +0100

    Initialize and setup Blog react project

---
+++ blog/src/containers/Blog/Blog.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
+
+class Blog extends Component {
+    state = {
+        posts: [],
+        selectedPosstId: null,
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then((res) => {
+                const posts = res.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                })
+                this.setState({ posts: updatedPosts });
+            })
+
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({ selectedPosstId: id });
+    }
+
+    render() {
+        const posts = this.state.posts.map(post => {
+            return <Post
+                key={post.id}
+                title={post.title}
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)}
+            />
+        })
+
+        return (
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <section>
+                    <FullPost id={this.state.selectedPosstId} />
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
