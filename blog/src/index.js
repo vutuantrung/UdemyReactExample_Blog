2ccf7a1359e21d315dc611b3785499114689c6db
VUTuanTrung <trung.lespace@gmail.com>
2021-01-04 13:30:38 +0100

    working with axios interceptors

--- blog/src/index.js
+++ blog/src/index.js
@@ -3,6 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+axios.interceptors.request.use(
+    (req) => {
+        console.log(req);
+        // Edit request config (Ex: Add some comments, headers, authorize header...)
+
+        return req;
+    },
+    (error) => {
+        // This error here related to sending the request (Ex: no internet)
+        console.log(error);
+        return Promise.reject(error);
+    });
+
+
+axios.interceptors.response.use(
+    (req) => {
+        console.log(req);
+        // Edit request configs
+
+        return req;
+    },
+    (error) => {
+        // This error here related to sending the request (Ex: no internet)
+        console.log(error);
+        return Promise.reject(error);
+    });
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
