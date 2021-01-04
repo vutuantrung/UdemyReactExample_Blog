cc80cffddca07bc54dee3fcd0995de91174eac4b
VUTuanTrung <trung.lespace@gmail.com>
2021-01-04 16:44:57 +0100

    using Route

--- blog/src/containers/Blog/Blog.js
+++ blog/src/containers/Blog/Blog.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 //import axios from 'axios';
 import './Blog.css';
 import Posts from './Posts/Posts';
+import { Route } from 'react-router-dom';
 
 class Blog extends Component {
 
@@ -17,7 +18,8 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts />
+                <Route path='/' exact component={Posts} />
+                {/* <Route path='/' exact render={Posts} Component={Posts} /> */}
             </div>
         );
     }
