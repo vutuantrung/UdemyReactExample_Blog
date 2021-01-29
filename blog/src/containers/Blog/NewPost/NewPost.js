c23060347fca6bb5231737e3bab42ac84b542dc6
VU Tuan Trung <trung.lespace@gmail.com>
2021-01-29 12:11:24 +0100

    moving project to main project's sub folder

--- blog/src/containers/Blog/NewPost/NewPost.js
+++ blog/src/containers/Blog/NewPost/NewPost.js
@@ -11,21 +11,22 @@ class NewPost extends Component {
         author: 'Max',
         error: false,
         submitted: false,
-    }
+    };
 
     postDataHandler = () => {
         const data = {
             title: this.state.title,
             body: this.state.body,
             author: this.state.author,
-        }
+        };
 
-        axios.post('/posts', data)
-            .then((res) => {
+        axios
+            .post('/posts', data)
+            .then(res => {
                 if (res.status === 201) alert('Success.');
                 //this.setState({ submitted: true });
             })
-            .catch((err) => {
+            .catch(err => {
                 this.setState({ error: true });
                 console.log(err);
             })
@@ -36,8 +37,8 @@ class NewPost extends Component {
                 // This code will replace history stack
                 // new-post then new-post gonna be replaced by post, so we cannot go back
                 this.props.history.replace('/posts');
-            })
-    }
+            });
+    };
 
     componentDidMount() {
         console.log(this.props);
@@ -46,18 +47,29 @@ class NewPost extends Component {
     render() {
         let redirect = null;
         if (this.state.submitted) {
-            redirect = <Redirect to='posts' />;
+            redirect = <Redirect to="posts" />;
         }
         return (
             <div className="NewPost">
                 {redirect}
                 <h1>Add a Post</h1>
                 <label>Title</label>
-                <input type="text" value={this.state.title} onChange={(event) => this.setState({ title: event.target.value })} />
+                <input
+                    type="text"
+                    value={this.state.title}
+                    onChange={event => this.setState({ title: event.target.value })}
+                />
                 <label>Content</label>
-                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({ content: event.target.value })} />
+                <textarea
+                    rows="4"
+                    value={this.state.content}
+                    onChange={event => this.setState({ content: event.target.value })}
+                />
                 <label>Author</label>
-                <select value={this.state.author} onChange={(event) => this.setState({ author: event.target.value })}>
+                <select
+                    value={this.state.author}
+                    onChange={event => this.setState({ author: event.target.value })}
+                >
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
